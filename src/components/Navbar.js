f3103466fd1e407fbc1be88e96783403170242ef
Shruti Malik <shruti.malik.ramaswamy@gmail.com>
2021-12-16 10:01:09 -0600

    logo added

---
+++ src/components/Navbar.js
@@ -0,0 +1,8 @@
+import logo from '../images/airbnb-logo.png';
+import '../App.css';
+
+export default function Navbar(){
+    return(
+        <img src={logo} className="App-logo" alt="logo" />
+    )
+}
